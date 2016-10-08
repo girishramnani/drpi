5871a606a9a263378e8f5e4cbf1b6d51dbb76bad
shivam <you@example.com>
2016-10-08 18:55:11 +0530

    changed the events for two sensors

---
+++ README.md
@@ -0,0 +1,15 @@
+# iot-starter
+iot starter express app for arduino.
+
+<br>
+used for getting temperature from arduino 
+
+```
+npm install
+nodejs index.js
+```
+
+<br>
+The server is running on `8000` and the socket.io is listening on `8001`.
+also dont forget to change the ip address from `my ip` to urs in the `index.html`
+
---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var io = require('socket.io')(8001);
+var server = require('http').Server(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.use(express.static('public'));
+
+server.listen(8000,function(err){ console.log("listening on 8000") });
+
+app.get('/',function(req,res) { res.sendFile(__dirname+"/index.html") });
+
+app.post("/api",function(req,res) {
+    console.log(req.body.data);
+	if (req.body.intensity){
+	io.emit('intensity',{ data: req.body.intensity} )	
+	} else if (req.body.temprature) {
+	io.emit('temprature',{ data: req.body.temprature });	
+	}
+	 
+    
+    res.end("received");
+});
+
+
